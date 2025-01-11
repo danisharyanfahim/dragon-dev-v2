5ee3acb94930f315201ca6c01060f3336ce6856c
Danish Fahim <danisharyanfahim@gmail.com>
2025-01-11 16:31:23 -0500

    fixed bug with footer icon styling and deleted empty folders

--- app/global/variables.ts
+++ app/global/variables.ts
@@ -12,16 +12,16 @@ export const footerInfo = [
   {
     text: "danisharyanfahim@gmail.com",
     name: "email",
-    iconUrl: "static/icons/footer-icons/email-icon.png",
+    iconUrl: "/static/icons/footer-icons/email-icon.png",
   },
   {
     text: "647-767-7829",
     name: "phone",
-    iconUrl: "static/icons/footer-icons/phone-icon.png",
+    iconUrl: "/static/icons/footer-icons/phone-icon.png",
   },
   {
     text: "Toronto, ON",
     name: "address",
-    iconUrl: "static/icons/footer-icons/address-icon.png",
+    iconUrl: "/static/icons/footer-icons/address-icon.png",
   },
 ];
