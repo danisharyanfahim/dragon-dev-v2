ff7a68922dde4bcb36ebc6dbe29450eac88fbbca
Danish Fahim <danisharyanfahim@gmail.com>
2025-01-22 23:33:25 -0500

    updated site headers

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,23 @@ export default function RootLayout({
         <meta name="description" content="DragonDev.ca" />
         <meta name="author" content="Danish Fahim" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>Dragon Dev</title>
+
+        <meta property="og:title" content="DragonDev.ca" />
+        <meta
+          property="og:description"
+          content="DragonDev.ca is the portfolio website of Danish Fahim."
+        />
+        <meta
+          property="og:image"
+          content="https://dragondev.ca/public/static/icons/dragon-dev.svg"
+        />
+        <meta property="og:url" content="https://dragondev.ca/" />
+        <meta property="og:type" content="website" />
+        <meta property="og:site_name" content="DragonDev.ca" />
+        <meta property="og:locale" content="en_US" />
+        <meta property="og:author" content="Dragon Dev" />
+        <meta property="og:published_time" content="2025-01-22T10:00:00Z" />
+        <title>DragonDev.ca</title>
       </head>
       <body>
         <ThemeProvider
